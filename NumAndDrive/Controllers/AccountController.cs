3e93ae3fe7e10b0e119a13c83b04f7c1bf53ce0a
mathiasdumas <mathiasdumaspro@gmail.com>
2024-05-30 20:16:19 +0200

    started but not finished first login of an user cause im hungry

--- NumAndDrive/Controllers/AccountController.cs
+++ NumAndDrive/Controllers/AccountController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using NumAndDrive.Database;
 using NumAndDrive.Models;
 using NumAndDrive.ViewModels;
 
@@ -18,10 +19,14 @@ namespace NumAndDrive.Controllers
     public class AccountController : Controller
     {
         private readonly SignInManager<User> _signInManager;
+        private readonly UserManager<User> _userManager;
+        private readonly NumAndDriveDbContext Db;
 
-        public AccountController(SignInManager<User> signInManager)
+        public AccountController(SignInManager<User> signInManager, UserManager<User> userManager, NumAndDriveDbContext db)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
+            Db = db;
         }
 
         [HttpGet]
@@ -44,6 +49,13 @@ namespace NumAndDrive.Controllers
                 var result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, model.RememberMe, lockoutOnFailure: false);
                 if (result.Succeeded)
                 {
+                    //string userId = _userManager.GetUserId(User);
+                    //var user = Db.Users.Find(userId);
+                    //if (user.IsFirstLogin == 0)
+                    //{
+                    //    user.IsFirstLogin = 1;
+                    //    return View("FirstLogin");
+                    //}
                     return LocalRedirect(returnUrl ?? Url.Action("Index", "Home"));
                 }
                 else
