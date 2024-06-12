fd415336ec77282811d681b2694381f9094f3208
Jessica GUALTIERI <jessicagualtieri@hotmail.fr>
2024-06-12 09:19:12 +0200

    Fixed: add a data validation on the creating a journey viewmodel

--- NumAndDrive/ViewModels/JourneyCompanyViewModel.cs
+++ NumAndDrive/ViewModels/JourneyCompanyViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using NumAndDrive.Models;
 
 namespace NumAndDrive.ViewModels
@@ -8,7 +9,6 @@ namespace NumAndDrive.ViewModels
 		public Journey Journey { get; set; }
 		public IEnumerable<Company> Company { get; set; }
 		public string AddressToTrim { get; set; }
-
         public string AddressDeparting { get; set; }
         public string AddressIncoming { get; set; }
         public string CompanyName { get; set; }
@@ -16,6 +16,7 @@ namespace NumAndDrive.ViewModels
 		public string UserLastName { get; set; }
 		public DateOnly DepartureDate { get; set; }
 		public TimeOnly DepartureHour { get; set; }
+		[Range(1,4)]
 		public int AvailableSpots { get; set; }
 		public int JourneyId { get; set; }
 
