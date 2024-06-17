85beefb189400f98bec5358cc0efc881609069a1
Jessica GUALTIERI <jessicagualtieri@hotmail.fr>
2024-06-17 14:16:36 +0200

    Fixed: all methods are now used in the interface

--- NumAndDrive/Models/Repositories/IAdminRepository.cs
+++ NumAndDrive/Models/Repositories/IAdminRepository.cs
@@ -6,10 +6,19 @@ namespace NumAndDrive.Models.Repositories
 {
 	public interface IAdminRepository
 	{
-        Task UploadUsersFromCSVFile(Admin admin);
+        Task UploadUsersFromCSVFile(AdminViewModel adminViewModel);
         string PasswordGenerator();
         bool IsUserValid(User user);
         bool IsEditUserViewModelValid(EditUserViewModel editUserViewModel);
+        int GetNumberOfUsersInDatabase();
+        List<User> GetUsers();
+        List<User> GetUsersByName(string name);
+        User GetUserDetails(string id);
+        Task ArchiveUser(User user);
+        Task CreateSingleUser(CreateUserViewModel userViewModel);
+        List<Status> GetStatuses();
+        List<Department> GetDepartments();
+        Task EditUser(EditUserViewModel newValues, User user);
     }
 }
 
