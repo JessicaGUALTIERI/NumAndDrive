0adbfa9429801d0872febb1a0ba4d058b6ee73d8
Jessica GUALTIERI <jessicagualtieri@hotmail.fr>
2024-06-17 14:17:04 +0200

    Fixed: controller is now clean, only methods from the repository are used

--- NumAndDrive/Controllers/AdminController.cs
+++ NumAndDrive/Controllers/AdminController.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Formats.Asn1;
-using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using NumAndDrive.Database;
 using NumAndDrive.Models;
 using NumAndDrive.Models.Repositories;
 using NumAndDrive.ViewModels;
@@ -17,117 +9,91 @@ namespace NumAndDrive.Controllers
     [Authorize(Roles = "Admin")]
     public class AdminController : Controller
     {
-
-        public IFormFile? File { get; set; }
-        private readonly UserManager<User> _userManager;
-        private readonly NumAndDriveDbContext Db;
         private readonly IAdminRepository _adminRepository;
  
-        public AdminController(NumAndDriveDbContext db, UserManager<User> userManager, IAdminRepository adminRepository)
+        public AdminController(IAdminRepository adminRepository)
         {
-            Db = db;
-            _userManager = userManager;
             _adminRepository = adminRepository;
         }
 
         public IActionResult Index()
         {
-            return View();
+            AdminViewModel admin = new AdminViewModel { NumberOfUsers = _adminRepository.GetNumberOfUsersInDatabase() };
+            return View(admin);
         }
 
         public IActionResult GetUsers()
         {
-            IEnumerable<User> users = Db.Users.Where(x => x.ArchiveDate == null).ToList();
+            List<User> users = _adminRepository.GetUsers();
             return View(users);
         }
 
         [HttpPost]
         public IActionResult GetUsersByName(string searchString)
         {
-            Console.WriteLine(searchString);
-            var user = Db.Users.Where(x => x.FirstName.ToLower().Contains(searchString.ToLower()) || x.LastName.ToLower().Contains(searchString.ToLower()));
-            return View(user.ToList());
+            List<User> user = _adminRepository.GetUsersByName(searchString);
+            return View(user);
         }
 
         public IActionResult Details(string id)
         {
-            User user = Db.Users.Find(id);
+            User user = _adminRepository.GetUserDetails(id);
             return View(user);
         }
 
         public IActionResult Edit(string id)
         {
-            User user = Db.Users.Find(id);
+            User user = _adminRepository.GetUserDetails(id);
             EditUserViewModel editUserViewModel = new EditUserViewModel {
                 Id = id,
                 FirstName = user.FirstName,
                 LastName = user.LastName,
                 PhoneNumber = user.PhoneNumber,
-                Statuses = Db.Statuses,
-                Departments = Db.Departments
+                Statuses = _adminRepository.GetStatuses(),
+                Departments = _adminRepository.GetDepartments()
             };
             return View(editUserViewModel);
         }
 
         [HttpPost]
-        public IActionResult Edit(EditUserViewModel user, string id)
+        public IActionResult Edit(EditUserViewModel newUser, string id)
         {
-            if (ModelState.IsValid && user != null && _adminRepository.IsEditUserViewModelValid(user))
+            if (ModelState.IsValid && newUser != null && _adminRepository.IsEditUserViewModelValid(newUser))
             {
-                User ToFindUser = Db.Users.Find(id);
+                User user = _adminRepository.GetUserDetails(id);
 
-                if (ToFindUser != null)
+                if (user != null)
                 {
-                    ToFindUser.LastName = user.LastName;
-                    ToFindUser.FirstName = user.FirstName;
-                    ToFindUser.PhoneNumber = user.PhoneNumber;
-                    Db.SaveChanges();
+                    _adminRepository.EditUser(newUser, user);
                 }
-
                 return RedirectToAction(nameof(GetUsers));
             }
-            return View(user);
-                
+            return View(newUser);
         }
 
         public IActionResult Archive(string id)
         {
-            User user = Db.Users.Find(id);
+            User user = _adminRepository.GetUserDetails(id);
 
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            user.FirstName = "utilisateur";
-            user.LastName = "utilisateur";
-            user.Email = null;
-            user.NormalizedEmail = null;
-            user.PhoneNumber = null;
-            user.Email = null;
-            user.UserName = "utilisateur";
-            user.NormalizedUserName = "UTILISATEUR";
-
-            user.ArchiveDate = DateOnly.FromDateTime(DateTime.UtcNow);
-            Db.SaveChanges();
+            _adminRepository.ArchiveUser(user);
 
             return RedirectToAction(nameof(GetUsers));
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> UploadUsersFromCSVFile(Admin admin)
+        public async Task<IActionResult> UploadUsersFromCSVFile(AdminViewModel admin)
         {
-            await _adminRepository.UploadUsersFromCSVFile(admin);
-            return RedirectToAction(nameof(Index));
+           await _adminRepository.UploadUsersFromCSVFile(admin);
+           return RedirectToAction(nameof(Index));
         }
 
         public IActionResult CreateSingleUser()
         {
             CreateUserViewModel createSingleUserViewModel = new CreateUserViewModel
             {
-                Statuses = Db.Statuses,
-                Departments = Db.Departments
+                Statuses = _adminRepository.GetStatuses(),
+                Departments = _adminRepository.GetDepartments()
             };
             return View(createSingleUserViewModel);
         }
@@ -138,25 +104,11 @@ namespace NumAndDrive.Controllers
         {
             if (ModelState.IsValid)
             {
-                User user = new User
-                {
-                    LastName = userViewModel.LastName,
-                    FirstName = userViewModel.FirstName,
-                    Email = userViewModel.Email,
-                    PhoneNumber = userViewModel.PhoneNumber,
-                    StatusId = userViewModel.StatusId,
-                    DepartmentId = userViewModel.DepartmentId,
-                    UserTypeId = 11,
-                    ProfilePicturePath = "/img/profile-pic-blue.png",
-                    UserName = userViewModel.Email
-                };
-                string password = _adminRepository.PasswordGenerator();
-                await _userManager.CreateAsync(user, password);
-                await _userManager.AddToRoleAsync(user, "User");
+                await _adminRepository.CreateSingleUser(userViewModel);
                 return RedirectToAction(nameof(Index));
             }
-            userViewModel.Statuses = Db.Statuses.ToList();
-            userViewModel.Departments = Db.Departments.ToList();
+            userViewModel.Statuses = _adminRepository.GetStatuses();
+            userViewModel.Departments = _adminRepository.GetDepartments();
             return View(userViewModel);
         }
     }
